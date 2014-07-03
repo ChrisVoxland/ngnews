31ceb1f7c3f3330946abe1c42e7d174dbf4bb405
chrisvoxland <chrisvoxland@gmail.com>
2014-07-03 14:01:01 -0400

    add voting, comments and other fun stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,19 +12,34 @@ var app = angular.module('ngnewsApp', [
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ]);
+    'ngTouch',
+    'firebase'
+  ])
   app.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/posts.html',
         controller: 'PostsCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/users/:username', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
       })
       .otherwise({
         redirectTo: '/'
-      });
+      })
   });
+
+  app.constant('FIREBASE_URL','https://burning-fire-4791.firebaseio.com/');
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,17 +1,33 @@
-app.controller('PostsCtrl', function($scope, Post) {
-  $scope.posts = Post.get();
+app.controller('PostsCtrl', function($scope, $location, Post) {
+  if ($location.path() === '/'){
+    $scope.posts = Post.all;
+  }
   $scope.post = {url: 'http://', title:''};
 
-  $scope.submitPost = function() {
-    Post.save($scope.post, function(ref) {
-      $scope.posts[ref.name] = $scope.post;
-      $scope.post = {url: 'http://', title:''};
-    });
+  $scope.deletePost = function(postId) {
+    Post.delete(postId);
   };
 
-  $scope.deletePost = function(postId) {
-    Post.delete({id: postId}, function() {
-      delete $scope.posts[postId];
-    });
+  $scope.upVotePost = function(postId, upVoted){
+    if (upVoted){
+      Post.clearVote(postId, upVoted);
+    } else {
+      Post.upVote(postId);
+    }
+  };
+  $scope.downVotePost = function(postId, downVoted){
+    if (downVoted) {
+      Post.clearVote(postId, !downVoted);
+    } else {
+      Post.downVote(postId);
+    }
+  };
+   
+  $scope.upVoted = function(post){
+    return Post.upVoted(post);
+  };
+   
+  $scope.downVoted = function(post){
+    return Post.downVoted(post);
   };
 });
